c6b6c46c82428ef5c165126f2cf4bf1332835685
StephanZaaijer <stephan.zaaijer@gmail.com>
2022-03-15 19:56:12 +0100

    updated size.sh paths

--- .github/workflows/size.sh
+++ .github/workflows/size.sh
@@ -5,13 +5,10 @@ for file in $files; do
     size=$(convert $file -print "%wx%h" /dev/null)
     if [ $size != "1920x1080" ]
     then
-        echo "wrong size" $size $file
         echo "wrong size" $size $file >> ../removed_files.txt
         rm -f $file
         wrong_files+=($file) 
-    else
-        echo $file
     fi
 done
 echo $wrong_files
-python3 ./update_json.py ${wrong_files[@]}
+python3 .github/workflows/update_json.py ${wrong_files[@]}
